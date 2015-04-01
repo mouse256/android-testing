43642bedc0f95c8e2d532abe243002146c4e0b7a
Tom Billiet <tom.billiet@technicolor.com>
2015-04-01 12:55:00 +0200

    Adapter to make issue 162662 clear

---
+++ unittesting/BasicUnitAndroidTest/app/instrumentTest/java/com/example/android/testing/unittesting/basicunitandroidtest/LogHistoryAndroidUnitTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.testing.unittesting.basicunitandroidtest;
+
+import android.test.AndroidTestCase;
+
+import java.io.IOException;
+import org.qeo.testframework.TestListener2;
+
+/**
+ * Tests that the parcelable interface is implemented correctly.
+ */
+public class LogHistoryAndroidUnitTest extends AndroidTestCase
+{
+
+    public void test123() throws IOException
+    {
+        TestListener2 tl;
+    }
+}
